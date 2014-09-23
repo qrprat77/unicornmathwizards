ddf836a55d99fa3b8adf620395c4c26e6f1f9648
GB Hoyt <qrprat77@gmail.com>
2014-09-23 02:23:36 -0400

    Writing the Create a unicorn lover profile

---
+++ features/User_creates_a_profile.feature
@@ -0,0 +1,10 @@
+Feature: When a new math wizard joins the game, they must choose certain things.
+
+As a person who loves unicorns
+I want to create a game profile
+So I can learn math and get more unicorns!
+
+Scenario: I create a brand new profile
+Given I love unicorns and want to learn math
+When I create a new unicorn lover profile
+Then I have a user name, a favorite type of unicorn, and I choose what kind of quiz I want to master first.
---
+++ features/step_definitions/make_profile_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I love unicorns and want to learn math$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I create a new unicorn lover profile$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I have a user name, a favorite type of unicorn, and I choose what kind of quiz I want to master first\.$/) do
+  pending # express the regexp above with the code you wish you had
+end
